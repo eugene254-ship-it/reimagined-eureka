f2c1054316c609c42b3e389f09167372494a4cb8
Michael Beebower <3438663+beebus@users.noreply.github.com>
2021-12-18 17:59:19 -0600

    Fixing dependency errors. Replaced an obsolete library in Particle.js.

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Particles from "react-particles-js";
+//import Particles from "react-particles-js";
+import Particles from "react-tsparticles";
 
 function Particle() {
   return (
